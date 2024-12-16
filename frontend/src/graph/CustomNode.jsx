be3dc65c810e44ce9b235b8c71506637fbba229e
darylldan <dccaponpon@up.edu.ph>
2024-12-17 06:15:16 +0800

    feat: added selected course button

--- frontend/src/graph/CustomNode.jsx
+++ frontend/src/graph/CustomNode.jsx
@@ -3,7 +3,7 @@ import { Handle, Position } from "@xyflow/react";
 function CustomNode({ data }) {
     console.log(`is selected for ${data.courseID} is ${data.status}`)
   return (
-    <div className={`flex flex-col h-14 w-32 justify-center items-center rounded-xl outline outline-2 outline-white ${data.status == 1 ? `bg-blue-700` : data.status == 2 ? `bg-amber-700` : `bg-slate-900`} hover:cursor-pointer ${data.status == 1 ? 'scale-110' : 'scale-100'} transition-all`}
+    <div className={`flex flex-col h-14 w-32 justify-center items-center rounded-xl outline outline-2 outline-white ${data.status == 1 ? `bg-blue-700` : data.status == 2 ? `bg-amber-700` : `bg-slate-900`} hover:cursor-pointer ${data.status == 1 ? 'scale-110' : 'scale-100'} transition-all ${data.status == 2 && 'pointer-events-none'}`}
         style={{strokeWidth: 10}}
     >
       <div className="text-white font-bold">{data.courseID}</div>
